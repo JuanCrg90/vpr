e05b5fce7c06e38b6068aca119cae3bb35b9c20d
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2019-07-10 16:22:43 -0500

    Update system calls to launchy API

--- lib/vpr/cli.rb
+++ lib/vpr/cli.rb
@@ -1,16 +1,17 @@
 require 'thor'
+require 'launchy'
 require 'vpr/url'
 
 module Vpr
   class CLI < Thor
     desc 'visit COMMIT', 'visit the commit in github'
     def visit(commit)
-      system('open', Url.commit_url(commit))
+      Launchy.open(Url.commit_url(commit))
     end
 
     desc 'search COMMIT', 'search the commit in github'
     def search(commit)
-      system('open', Url.search_url(commit))
+      Launchy.open(Url.search_url(commit))
     end
   end
 end
