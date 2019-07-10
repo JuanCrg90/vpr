d0498474eca0c5de262b4b38f485fc14fc781d0f
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2019-07-10 16:31:02 -0500

    fix broken tests

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,16 +1,17 @@
 require 'spec_helper'
+require 'launchy'
 
 RSpec.describe "CLI" do
   describe 'visit' do
     it 'open initial commit github page' do
-      expect_any_instance_of(Kernel).to receive(:system).with("open", "https://github.com/JuanCrg90/vpr/commit/123")
+      expect(Launchy).to receive(:open).with("https://github.com/JuanCrg90/vpr/commit/123")
       Vpr::CLI.start(["visit", '123'])
     end
   end
 
   describe 'search' do
     it 'open initial commit github search page' do
-      expect_any_instance_of(Kernel).to receive(:system).with("open", "https://github.com/JuanCrg90/vpr/search?q=123")
+      expect(Launchy).to receive(:open).with("https://github.com/JuanCrg90/vpr/search?q=123")
       Vpr::CLI.start(["search", '123'])
     end
   end
