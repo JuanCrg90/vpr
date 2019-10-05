ad07043aeb24f5b65c60efc0743fa8ef2413e14e
Andrew Mason <andrewmcodes@protonmail.com>
2019-10-05 10:32:47 -0700

    run standardrb --fix on the project

--- Rakefile
+++ Rakefile
@@ -1,14 +1,14 @@
 require "bundler/gem_tasks"
 require "rspec/core/rake_task"
-require 'github_changelog_generator/task'
+require "github_changelog_generator/task"
 
 RSpec::Core::RakeTask.new(:spec)
 
-task :default => :spec
+task default: :spec
 
 GitHubChangelogGenerator::RakeTask.new :changelog do |config|
-  config.user = 'JuanCrg90'
-  config.project = 'vpr'
-  config.since_tag = '0.1.0'
-  config.future_release = '1.0.0'
+  config.user = "JuanCrg90"
+  config.project = "vpr"
+  config.since_tag = "0.1.0"
+  config.future_release = "1.0.0"
 end
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,77 +1,77 @@
-require 'spec_helper'
-require 'launchy'
+require "spec_helper"
+require "launchy"
 
 RSpec.describe "CLI" do
-  describe 'home' do
-    it 'open github homepage' do
+  describe "home" do
+    it "open github homepage" do
       url = %r{https://github.com/\w+/vpr}
       expect(Launchy).to receive(:open).with(url)
       Vpr::CLI.start(["home"])
     end
   end
 
-  describe 'pulls' do
-    it 'open github pull requests page' do
+  describe "pulls" do
+    it "open github pull requests page" do
       url = %r{https://github.com/\w+/vpr/pulls}
       expect(Launchy).to receive(:open).with(url)
       Vpr::CLI.start(["pulls"])
     end
   end
 
-  describe 'issues' do
-    it 'open github issues page' do
+  describe "issues" do
+    it "open github issues page" do
       url = %r{https://github.com/\w+/vpr/issues}
       expect(Launchy).to receive(:open).with(url)
       Vpr::CLI.start(["issues"])
     end
   end
 
-  describe 'branches' do
-    it 'open github branches page' do
+  describe "branches" do
+    it "open github branches page" do
       url = %r{https://github.com/\w+/vpr/branches}
       expect(Launchy).to receive(:open).with(url)
       Vpr::CLI.start(["branches"])
     end
   end
 
-  describe 'branch' do
-    it 'open github branch page' do
+  describe "branch" do
+    it "open github branch page" do
       url = %r{https://github.com/\w+/vpr/tree/\w+}
       expect(Launchy).to receive(:open).with(url)
       Vpr::CLI.start(["branch"])
     end
   end
 
-  describe 'pull' do
-    it 'open github current branch pull request page' do
+  describe "pull" do
+    it "open github current branch pull request page" do
       url = %r{https://github.com/\w+/vpr/pull/\w+}
       expect(Launchy).to receive(:open).with(url)
       Vpr::CLI.start(["pull"])
     end
   end
 
-  describe 'visit' do
-    it 'open initial commit github page' do
+  describe "visit" do
+    it "open initial commit github page" do
       url = %r{https://github.com/\w+/vpr/commit/123}
       expect(Launchy).to receive(:open).with(url)
-      Vpr::CLI.start(["visit", '123'])
+      Vpr::CLI.start(["visit", "123"])
     end
   end
 
-  describe 'search' do
-    it 'open initial commit github search page' do
+  describe "search" do
+    it "open initial commit github search page" do
       url = %r{https://github.com/\w+/vpr/search\?q=123}
       expect(Launchy).to receive(:open).with(url)
-      Vpr::CLI.start(["search", '123'])
+      Vpr::CLI.start(["search", "123"])
     end
   end
 
-  describe 'version' do
-    it 'shows gem version with double dash' do
+  describe "version" do
+    it "shows gem version with double dash" do
       expect(Vpr::CLI.start(["--version"])).to match("1.0.0")
     end
 
-    it 'shows gem version without double dash' do
+    it "shows gem version without double dash" do
       expect(Vpr::CLI.start(["version"])).to match("1.0.0")
     end
   end
