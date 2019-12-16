c6e0c9f706daacedfdc55347e0c57d8062af47b1
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2019-12-16 11:27:36 -0600

    Add Bitbucket Service class
    We want to separate responsibilities, this adds a new Bitbucket Class that
    only handles the Bitbucket repositories

--- lib/vpr/services.rb
+++ lib/vpr/services.rb
@@ -1,5 +1,6 @@
 module Vpr
   module Services
     autoload :GitHub, "vpr/services/github"
+    autoload :Bitbucket, "vpr/services/bitbucket"
   end
 end
---
+++ spec/services/bitbucket_spec.rb
@@ -0,0 +1,85 @@
+require "spec_helper"
+
+RSpec.describe Vpr::Services::Bitbucket do
+  let(:commit) { "30bd60" }
+
+  describe ".home_url" do
+    subject { described_class.home_url }
+
+    it "returns the project url" do
+      url = %r{https://bitbucket.org/\w+/vpr}
+      expect(Vpr::GitParser).to receive(:repo_url).and_return("https://bitbucket.org/JuanCrg90/vpr")
+      expect(subject).to match(url)
+    end
+  end
+
+  describe ".pulls_url" do
+    subject { described_class.pulls_url }
+
+    it "returns the commit url" do
+      url = %r{https://bitbucket.org/\w+/vpr/pull-requests}
+      expect(Vpr::GitParser).to receive(:repo_url).and_return("https://bitbucket.org/JuanCrg90/vpr")
+      expect(subject).to match(url)
+    end
+  end
+
+  describe ".issues_url" do
+    subject { described_class.issues_url }
+
+    it "returns the  issues url" do
+      url = %r{https://bitbucket.org/\w+/vpr/issues}
+      expect(Vpr::GitParser).to receive(:repo_url).and_return("https://bitbucket.org/JuanCrg90/vpr")
+      expect(subject).to match(url)
+    end
+  end
+
+  describe ".branches_url" do
+    subject { described_class.branches_url }
+
+    it "returns the  issues url" do
+      url = %r{https://bitbucket.org/\w+/vpr/branches}
+      expect(Vpr::GitParser).to receive(:repo_url).and_return("https://bitbucket.org/JuanCrg90/vpr")
+      expect(subject).to match(url)
+    end
+  end
+
+  describe ".branch_url" do
+    subject { described_class.branch_url }
+
+    it "returns the branch url" do
+      url = %r{https://bitbucket.org/\w+/vpr/branch/\w+}
+      expect(Vpr::GitParser).to receive(:repo_url).and_return("https://bitbucket.org/JuanCrg90/vpr")
+      expect(subject).to match(url)
+    end
+  end
+
+  describe ".pull_url" do
+    subject { described_class.pull_url }
+
+    it "returns the pull request" do
+      url = %r{https://bitbucket.org/\w+/vpr/pull-requests/new\?source=\w+}
+      expect(Vpr::GitParser).to receive(:repo_url).and_return("https://bitbucket.org/JuanCrg90/vpr")
+      expect(subject).to match(url)
+    end
+  end
+
+  describe ".commit_url" do
+    subject { described_class.commit_url(commit) }
+
+    it "returns the commit url" do
+      url = %r{https://bitbucket.org/\w+/vpr/commits/30bd60}
+      expect(Vpr::GitParser).to receive(:repo_url).and_return("https://bitbucket.org/JuanCrg90/vpr")
+      expect(subject).to match(url)
+    end
+  end
+
+  describe ".search_url" do
+    subject { described_class.search_url(commit) }
+
+    it "returns the search page url" do
+      url = %r{https://bitbucket.org/\w+/vpr/commits/all\?search=30bd60}
+      expect(Vpr::GitParser).to receive(:repo_url).and_return("https://bitbucket.org/JuanCrg90/vpr")
+      expect(subject).to match(url)
+    end
+  end
+end
